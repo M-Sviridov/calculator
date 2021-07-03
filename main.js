8642cb42ca1bc6fb03ffb9ce0456ed16d6f88247
Mathieu Sviridov <msviridov@pm.me>
2021-07-03 10:46:28 +1000

    Operation functions and operate function created

---
+++ main.js
@@ -0,0 +1,20 @@
+let add = (a, b) => a + b;
+let substract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+function operate(num1, num2) {
+    operator = prompt('Please enter an operator (+, -, *, /): ');
+
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return substract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        operator = prompt('Please enter a valid operator: ');
+    }
+};
