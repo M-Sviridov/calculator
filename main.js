410eb0bbddfedd1c97e04af22837adb5fbcd536a
Mathieu Sviridov <msviridov@pm.me>
2021-07-07 11:31:06 +1000

    Refactoring of code into individual functions for each different buttons behaviour, keyboard support added

--- main.js
+++ main.js
@@ -44,83 +44,44 @@ function operate(operator, num1, num2) {
         return num1 * num2;
     } else if (operator === '÷') {
         return num1 / num2;
-    };
+    }
 };
 
-numPad.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if (display.textContent === '0' && button.textContent === '0');
-        else if (display.textContent === '0' && button.textContent !== '0') {
-            display.textContent = button.textContent;
-        } else if (num1 !== undefined && num2 !== undefined) {
-            display.textContent = button.textContent;
-            num2 = undefined;
-        } else if (num1 !== undefined && once) {
-            display.textContent = button.textContent;
-            once = false;
-        } else {
-            display.textContent += button.textContent;
-        }
-        console.log(e.target);
-    });
-
-});
-
-// function addDot() {
-//     if (display.textContent.indexOf('.') > -1);
-//     else {
-//         display.textContent += '.';
-//     }
-// }
-
-dotButton.addEventListener('click', () => {
-    if (display.textContent.indexOf('.') > -1);
-    else {
-        display.textContent += '.';
-    }
-});
-
-operators.forEach(button => {
-    button.addEventListener('click', () => {
-        if (num1 === undefined && num2 === undefined) {
-            num1 = Number(display.textContent);
-            console.log(num1 + ' num1');
-            operator = button.textContent;
-        } else if (num1 !== undefined && num2 === undefined) {
-            num2 = Number(display.textContent);
-            console.log(num2 + ' num2');
-            display.textContent = operate(operator, num1, num2)
-            num1 = operate(operator, num1, num2);
-            operator = button.textContent;
-        } else {
-            operator = button.textContent;
-        }
-    });
-});
-
-acButton.addEventListener('click', () => {
-    display.textContent = 0;
-    num1 = undefined;
-    num2 = undefined;
-    once = true;
-});
-
-backspaceButton.addEventListener('click', () => {
+// Deletes one number from the display
+function deleteNumber() {
     display.textContent = display.textContent.slice(0, -1);
     if (display.textContent === '') {
         display.textContent = '0';
     }
-});
+}
 
-plusMinusButton.addEventListener('click', () => {
+// Adds a decimal point and makes sure only one can be present
+function addDot() {
+    if (display.textContent.indexOf('.') > -1);
+    else {
+        display.textContent += '.';
+    }
+}
+
+// Resets the calculator and all the data
+function reset() {
+    display.textContent = 0;
+    num1 = undefined;
+    num2 = undefined;
+    once = true;
+}
+
+// Turns a positive number into a negative, and vice versa
+function positiveNegative() {
     if (Number(display.textContent) > 0) {
         display.textContent = -Math.abs(Number(display.textContent));
     } else if (Number(display.textContent) < 0) {
         display.textContent = Math.abs(Number(display.textContent));
     }
-});
+}
 
-equalButton.addEventListener('click', () => {
+// Calling the operate() function, setting variables for next operation
+function equal() {
     if (num1 === undefined);
     else if (num1 !== undefined) {
         num2 = Number(display.textContent);
@@ -129,4 +90,119 @@ equalButton.addEventListener('click', () => {
         num2 = undefined;
         once = true;
     }
+}
+
+// Adds a single number to the calculator display
+function addNumber(button) {
+    if (display.textContent === '0' && button.textContent === '0');
+    else if (display.textContent === '0' && button.textContent !== '0') {
+        display.textContent = button.textContent;
+    } else if (num1 !== undefined && num2 !== undefined) {
+        display.textContent = button.textContent;
+        num2 = undefined;
+    } else if (num1 !== undefined && once) {
+        display.textContent = button.textContent;
+        once = false;
+    } else {
+        display.textContent += button.textContent;
+    }
+}
+
+// Calls the operate() function with the chosen operator
+function operation(button) {
+    if (num1 === undefined && num2 === undefined) {
+        num1 = Number(display.textContent);
+        console.log(num1 + ' num1');
+        operator = button.textContent;
+    } else if (num1 !== undefined && num2 === undefined) {
+        num2 = Number(display.textContent);
+        console.log(num2 + ' num2');
+        display.textContent = operate(operator, num1, num2)
+        num1 = operate(operator, num1, num2);
+        operator = button.textContent;
+    } else {
+        operator = button.textContent;
+    }
+}
+
+numPad.forEach(button => {
+    button.addEventListener('click', (e) => {
+        addNumber(button);
+    });
 });
+
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        operation(button);
+    });
+});
+
+dotButton.addEventListener('click', () => {
+    addDot();
+});
+
+acButton.addEventListener('click', () => {
+    reset();
+});
+
+backspaceButton.addEventListener('click', () => {
+    deleteNumber();
+});
+
+plusMinusButton.addEventListener('click', () => {
+    positiveNegative();
+});
+
+equalButton.addEventListener('click', () => {
+    equal();
+});
+
+// Adds keyboard support
+window.addEventListener('keydown', (e) => {
+    console.log(e);
+    switch (true) {
+        case e.key === 'Backspace':
+            deleteNumber();
+            break;
+
+        case e.key === '.':
+            addDot();
+            break;
+
+        case e.key === 'Escape':
+            reset();
+            break;
+
+        case e.key === 'Alt':
+            positiveNegative();
+            break;
+
+        case e.key === '=':
+            equal();
+            break;
+
+        case e.key === 'Enter':
+            equal();
+            break;
+
+        case e.key in numPad:
+            addNumber(numPad[e.key]);
+            break;
+
+        case e.key === '+':
+            operation(operators[0]);
+            break;
+
+        case e.key === '-':
+            operation(operators[1]);
+            break;
+
+        case e.key === '*':
+            operation(operators[2]);
+            break;
+
+        case e.key === '/':
+            operation(operators[3]);
+            break;
+    }
+})
