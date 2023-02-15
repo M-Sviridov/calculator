f4b77483b07f45eb9c78ad4484ff3d29dd3d1c50
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-02-15 12:53:46 +0700

    Create functions for operators and using them

---
+++ main.js
@@ -0,0 +1,5 @@
+const operate = (operator, x, y) => operator(x, y);
+const multiply = (x, y) => x * y;
+const subtract = (x, y) => x - y;
+const divide = (x, y) => x / y;
+const add = (x, y) => x + y;
