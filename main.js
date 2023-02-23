5871b972a0e097c426b726deb1f5f222c551a274
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-02-23 17:08:19 +1100

    Create functions to populate the display with digits only

--- main.js
+++ main.js
@@ -3,3 +3,69 @@ const multiply = (x, y) => x * y;
 const subtract = (x, y) => x - y;
 const divide = (x, y) => x / y;
 const add = (x, y) => x + y;
+
+const display = document.querySelector(".display-text");
+
+const clear = document.querySelector(".clear");
+const pm = document.querySelector(".pm");
+const percent = document.querySelector(".percent");
+
+const divides = document.querySelector(".divides");
+const times = document.querySelector(".times");
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const equal = document.querySelector(".equal");
+
+const zero = document.querySelector(".zero");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const dot = document.querySelector(".dot");
+
+const inputButtons = [
+  zero,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+];
+
+inputButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (
+      !checkInitialState(display.textContent, button.textContent) &&
+      display.textContent == "0"
+    ) {
+      display.textContent = button.textContent;
+    } else if (!checkInitialState(display.textContent, button.textContent)) {
+      display.textContent += button.textContent;
+    } else {
+      display.textContent = button.textContent;
+    }
+  });
+});
+
+function checkInitialState(displayContent, buttonContent) {
+  let initialState = true;
+
+  if (displayContent == "0" && buttonContent != "0") {
+    initialState = false;
+  } else if (displayContent == "0" && buttonContent == ".") {
+    initialState = false;
+  } else if (displayContent != "0") {
+    initialState = false;
+  }
+
+  return initialState;
+}
